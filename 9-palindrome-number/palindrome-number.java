c3c03597b3c33dc16ca52fdcadb268790f41b867
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-25 16:18:22 +0530

    Time: 6 ms (28.42%) | Memory: 44.4 MB (26.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,15 +1,43 @@
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//        if(x<0){
+//         return false;
+//        }
+//        int rev=0;
+//        int original=x;
+//        while(x>0){
+//         int digit=x%10;
+//         rev=(rev*10)+digit;
+//         x=x/10;
+//        }
+//        return original==rev;
+//     }
+// }
+
+// class Solution {
+//     public boolean isPalindrome(int x) {
+//         if(x<0){
+//             return false;
+//         }
+//         String str=Integer.toString(x);
+//         String rev="";
+//         for(int i=str.length()-1;i>=0;i--){
+//             char ch=str.charAt(i);
+//             rev=rev+ch;
+//         }
+//         return str.equals(rev);
+//     }
+// }
+
 class Solution {
     public boolean isPalindrome(int x) {
-       if(x<0){
-        return false;
-       }
-       int rev=0;
-       int original=x;
-       while(x>0){
-        int digit=x%10;
-        rev=(rev*10)+digit;
-        x=x/10;
-       }
-       return original==rev;
+        String str=Integer.toString(x);
+        int n=str.length();
+        for(int i=0;i<n/2;i++){
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
     }
 }
