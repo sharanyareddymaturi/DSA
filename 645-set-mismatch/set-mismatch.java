4ce22e2706bccc1f8d664cf2aec5bea4d57b6a09
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-25 20:21:17 +0530

    Time: 15 ms (13.45%) | Memory: 45.7 MB (44.01%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,19 +1,40 @@
+// class Solution {
+//     public int[] findErrorNums(int[] nums) {
+//        HashMap<Integer,Integer>hm=new HashMap<>();
+//        int missing=-1;
+//        int duplicate=-1;
+//        for(int i=0;i<nums.length;i++){
+//         hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+//        } 
+//        for(int i=1;i<=nums.length;i++){
+//         if(!hm.containsKey(i)){
+//             missing=i;
+//         }
+//         else if(hm.get(i)==2){
+//             duplicate=i;
+//         }
+//        }
+//        return new int[]{duplicate,missing};
+//     }
+// }
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-       HashMap<Integer,Integer>hm=new HashMap<>();
-       int missing=-1;
-       int duplicate=-1;
-       for(int i=0;i<nums.length;i++){
-        hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
-       } 
-       for(int i=1;i<=nums.length;i++){
-        if(!hm.containsKey(i)){
-            missing=i;
+        HashSet<Integer>hs=new HashSet<>();
+        int duplicate=-1;
+        int missing=-1;
+        for(int i=0;i<nums.length;i++){
+            if(!hs.contains(nums[i])){
+                hs.add(nums[i]);
+            }else{
+                duplicate=nums[i];
+            }
         }
-        else if(hm.get(i)==2){
-            duplicate=i;
+        for(int i=1;i<=nums.length;i++){
+            if(!hs.contains(i)){
+                missing=i;
+            }
         }
-       }
-       return new int[]{duplicate,missing};
+        return new int[]{duplicate,missing};
     }
 }
