9c234668495e3ca6419de40d62713c17d0c56639
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-26 13:57:12 +0530

    Time: 0 ms (100%), Space: 45.1 MB (93.65%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -9,15 +9,15 @@
 //         return nums.length;
 //     }
 // }
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int expectedSum = (n * (n + 1)) / 2;
-        int actualSum = 0;
-        for (int num : nums) {
-            actualSum += num;
+        int n=nums.length;
+        int sum=(n*(n+1))/2;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=nums[i];
         }
-        return expectedSum - actualSum;
+        return sum-ans;
     }
 }
-
