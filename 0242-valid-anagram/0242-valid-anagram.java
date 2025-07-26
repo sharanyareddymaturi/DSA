d45550b4c23ca5a00b802893d2d5169ed4d48504
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-26 11:17:19 +0530

    Time: 13 ms (29.78%), Space: 45.2 MB (6.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,41 +1,41 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         HashMap<Character,Integer>hm=new HashMap<>();
-//         HashMap<Character,Integer>hm1=new HashMap<>();
-//         if(s.length()!=t.length()){
-//             return false;
-//         }
-//         for(char c:s.toCharArray()){
-//             hm.put(c,hm.getOrDefault(c,0)+1);
-//         }
-//         for(char ch:t.toCharArray()){
-//             hm1.put(ch,hm1.getOrDefault(ch,0)+1);
-//         }
-//         return hm.equals(hm1);
-//     }
-// }
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        
-        if (s.length() != t.length()) {
+        HashMap<Character,Integer>hm=new HashMap<>();
+        HashMap<Character,Integer>hm1=new HashMap<>();
+        if(s.length()!=t.length()){
             return false;
         }
-
-        for (char ch : s.toCharArray()) {
-            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        for(char c:s.toCharArray()){
+            hm.put(c,hm.getOrDefault(c,0)+1);
         }
-
-        for (char ch : t.toCharArray()) {
-            if (!hm.containsKey(ch)) {
-                return false;
-            }
-            hm.put(ch, hm.get(ch) - 1);
-            if (hm.get(ch) == 0) {
-                hm.remove(ch);
-            }
+        for(char ch:t.toCharArray()){
+            hm1.put(ch,hm1.getOrDefault(ch,0)+1);
         }
-
-        return hm.isEmpty();
+        return hm.equals(hm1);
     }
 }
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         HashMap<Character, Integer> hm = new HashMap<>();
+        
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+
+//         for (char ch : s.toCharArray()) {
+//             hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+//         }
+
+//         for (char ch : t.toCharArray()) {
+//             if (!hm.containsKey(ch)) {
+//                 return false;
+//             }
+//             hm.put(ch, hm.get(ch) - 1);
+//             if (hm.get(ch) == 0) {
+//                 hm.remove(ch);
+//             }
+//         }
+
+//         return hm.isEmpty();
+//     }
+// }
