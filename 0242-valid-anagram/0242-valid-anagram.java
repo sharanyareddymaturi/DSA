2f7a4686f1d4d8bb8bc83f5d6eb7e4e0f2912f08
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-26 11:07:39 +0530

    Time: 13 ms (29.78%), Space: 44.8 MB (25.19%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer>hm=new HashMap<>();
+        HashMap<Character,Integer>hm1=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(char c:s.toCharArray()){
+            hm.put(c,hm.getOrDefault(c,0)+1);
+        }
+        for(char ch:t.toCharArray()){
+            hm1.put(ch,hm1.getOrDefault(ch,0)+1);
+        }
+        return hm.equals(hm1);
+    }
+}
