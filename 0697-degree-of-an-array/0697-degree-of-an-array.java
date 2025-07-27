9664cb2093e347e3843a144f39221ac08c6322bb
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-27 18:39:07 +0530

    Time: 30 ms (28.78%), Space: 48.1 MB (33.56%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -4,17 +4,20 @@ class Solution {
         HashMap<Integer,Integer> hm=new HashMap<>();
         int degree=0;
         
-        HashMap<Integer,Integer> first_index=new HashMap<>();
+        HashMap<Integer,Integer> first_seen=new HashMap<>();
         int min_length=0;
         
         for(int i=0;i<n;i++){
-            first_index.putIfAbsent(nums[i],i);
+            first_seen.putIfAbsent(nums[i],i);
+
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+
             if(hm.get(nums[i])>degree){
                 degree=hm.get(nums[i]);
-                min_length=i-first_index.get(nums[i])+1;
+                min_length=i-first_seen.get(nums[i])+1;
+
             }else if(hm.get(nums[i])==degree){
-                min_length=Math.min(min_length,i-first_index.get(nums[i])+1);
+                min_length=Math.min(min_length,i-first_seen.get(nums[i])+1);
             }
         }
         return min_length;
