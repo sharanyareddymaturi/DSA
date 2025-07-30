8426bf59cdead1fff463cd5efc3da315bb07ea1b
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-07-30 21:52:23 +0530

    Time: 1 ms (42.9%), Space: 40.9 MB (55.17%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,11 +1,25 @@
+// class Solution {
+//     public int addDigits(int num) {
+//         while(num>=10){
+//             int sum=0;
+//             while(num!=0){
+//                 int digit=num%10;
+//                 sum=sum+digit;
+//                 num=num/10;
+//             }
+//             num=sum;
+//         }
+//         return num;
+//     }
+// }
+
 class Solution {
     public int addDigits(int num) {
         while(num>=10){
+            String str=Integer.toString(num);
             int sum=0;
-            while(num!=0){
-                int digit=num%10;
-                sum=sum+digit;
-                num=num/10;
+            for(int i=0;i<str.length();i++){
+                sum=sum+(str.charAt(i)-'0');
             }
             num=sum;
         }
