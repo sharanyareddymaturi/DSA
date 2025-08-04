3fc5ef4aa53aebb4e0954ff2a683739a0937cbb9
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-04 19:58:55 +0530

    Time: 7 ms (5.51%), Space: 43.1 MB (8.14%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.java
@@ -1,13 +1,38 @@
+// class Solution {
+//     public int countPairs(int[] nums, int k) {
+//         int count=0;
+//         for(int i=0;i<=nums.length-1;i++){
+//             for(int j=i+1;j<=nums.length-1;j++){
+//                 if(nums[i]==nums[j] && (i*j)%k==0){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
 class Solution {
     public int countPairs(int[] nums, int k) {
-        int count=0;
-        for(int i=0;i<=nums.length-1;i++){
-            for(int j=i+1;j<=nums.length-1;j++){
-                if(nums[i]==nums[j] && (i*j)%k==0){
-                    count++;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], x -> new ArrayList<>()).add(i);
+        }
+
+        for (List<Integer> indices : map.values()) {
+            for (int i = 0; i < indices.size() - 1; i++) {
+                for (int j = i + 1; j < indices.size(); j++) {
+                    if (((long) indices.get(i) * indices.get(j)) % k == 0) {
+                        count++;
+                    }
                 }
             }
         }
+
         return count;
     }
 }
