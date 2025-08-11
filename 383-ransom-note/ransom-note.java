71140d3c786ad0927a2129cb85e879a1c80f76c2
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-11 22:55:30 +0530

    Time: 10 ms (53.83%) | Memory: 45.4 MB (26.13%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,47 +1,45 @@
 // class Solution {
-//       public void ascii(String a) {
+//     public void ascii(String a) {
 //         System.out.print("ASCII of \"" + a + "\": ");
 //         for (int i = 0; i < a.length(); i++) {
 //             System.out.print((int) a.charAt(i) + " ");
 //         }
-//         System.out.println(); // for new line
+//         System.out.println();
 //     }
+
 //     public boolean canConstruct(String ransomNote, String magazine) {
-//         int i=0;
-//         int j=0;
-//         while(i<ransomNote.length() && j<magazine.length()){
-//             if(ransomNote.charAt(i)==magazine.charAt(j)){
-//                 i++;
-//             }
-//             j++;
+//         int[] freq = new int[26]; // For lowercase a-z
+
+//         // Count frequency of each character in magazine
+//         for (char c : magazine.toCharArray()) {
+//             freq[c - 'a']++;
 //         }
-//         return i==ransomNote.length();
+
+//         // Subtract frequency for each character in ransomNote
+//         for (char c : ransomNote.toCharArray()) {
+//             if (freq[c - 'a'] == 0) {
+//                 return false; // Not enough of this character
+//             }
+//             freq[c - 'a']--;
+//         }
+
+//         return true;
 //     }
 // }
 
 class Solution {
-    public void ascii(String a) {
-        System.out.print("ASCII of \"" + a + "\": ");
-        for (int i = 0; i < a.length(); i++) {
-            System.out.print((int) a.charAt(i) + " ");
-        }
-        System.out.println();
-    }
-
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] freq = new int[26]; // For lowercase a-z
+        HashMap<Character, Integer> magaHash = new HashMap<>();
 
-        // Count frequency of each character in magazine
         for (char c : magazine.toCharArray()) {
-            freq[c - 'a']++;
+            magaHash.put(c, magaHash.getOrDefault(c, 0) + 1);
         }
 
-        // Subtract frequency for each character in ransomNote
         for (char c : ransomNote.toCharArray()) {
-            if (freq[c - 'a'] == 0) {
-                return false; // Not enough of this character
+            if (!magaHash.containsKey(c) || magaHash.get(c) <= 0) {
+                return false;
             }
-            freq[c - 'a']--;
+            magaHash.put(c, magaHash.get(c) - 1);
         }
 
         return true;
