a8cfde79526ef0d3f3ecb2f1d7b6af841b6c6dfd
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-11 21:08:23 +0530

    Time: 0 ms (100%), Space: 41.6 MB (72.29%) - LeetHub

--- 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
+++ 2243-check-if-all-as-appears-before-all-bs/2243-check-if-all-as-appears-before-all-bs.java
@@ -1,8 +1,19 @@
+// class Solution {
+//     public boolean checkString(String s) {
+//         char ch[]=s.toCharArray();
+//         for(int i=1;i<ch.length;i++){
+//             if(ch[i-1]=='b' && ch[i]=='a'){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
+
 class Solution {
     public boolean checkString(String s) {
-        char ch[]=s.toCharArray();
-        for(int i=1;i<ch.length;i++){
-            if(ch[i-1]=='b' && ch[i]=='a'){
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i-1)=='b' && s.charAt(i)=='a'){
                 return false;
             }
         }
