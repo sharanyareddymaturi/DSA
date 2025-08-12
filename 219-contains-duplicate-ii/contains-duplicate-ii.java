5144df7dd7b42f876aa34311d16281adde186876
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-12 22:09:04 +0530

    Time: 1163 ms (8.74%) | Memory: 55.5 MB (99.24%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,12 +1,41 @@
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         HashMap<Integer,Integer>hm=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             int val=nums[i];
+//             if(hm.containsKey(val) && i-hm.get(val) <= k){
+//                 return true;
+//             }
+//             hm.put(val,i);
+//         }
+//         return false;
+//     }
+// }
+
+// class Solution {
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         for(int i=0;i<nums.length-1;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j]){
+//                     int temp=Math.abs(j-i);
+//                     if(temp<=k){
+//                         return true;
+//                     }
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int val=nums[i];
-            if(hm.containsKey(val) && i-hm.get(val) <= k){
-                return true;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length && (j - i) <= k; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
             }
-            hm.put(val,i);
         }
         return false;
     }
