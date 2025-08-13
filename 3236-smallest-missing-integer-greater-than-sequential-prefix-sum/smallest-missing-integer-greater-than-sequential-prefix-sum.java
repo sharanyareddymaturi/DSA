5d8d4bbeb98b0821ebe6f1dbd9e61b5d2d96ae1b
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-13 19:19:00 +0530

    Time: 1 ms (85.40%) | Memory: 42.1 MB (75.18%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,28 +1,48 @@
+// class Solution {
+//     public int missingInteger(int[] nums) {
+//         int n=nums.length;
+//         ArrayList<Integer>arr=new ArrayList<>();
+//         for(int num:nums){
+//             arr.add(num);
+//         }
+//         ArrayList<Integer>sequential=new ArrayList<>();
+//         sequential.add(arr.get(0));
+//         for(int i=0;i<n-1;i++){
+//             if(arr.get(i)+1 == arr.get(i+1)){
+//                 sequential.add(arr.get(i+1));
+//             }else{
+//                 break;
+//             }
+//         }
+//         int sum1=0;
+//         for(int sum:sequential){
+//             sum1+=sum;
+//         }
+//         HashSet<Integer>hs=new HashSet<>(arr);
+//         int ans=sum1;
+//         while(hs.contains(ans)){
+//            ans++;
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int missingInteger(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer>arr=new ArrayList<>();
-        for(int num:nums){
-            arr.add(num);
-        }
-        ArrayList<Integer>sequential=new ArrayList<>();
-        sequential.add(arr.get(0));
-        for(int i=0;i<n-1;i++){
-            if(arr.get(i)+1 == arr.get(i+1)){
-                sequential.add(arr.get(i+1));
+        int sum=nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]+1 == nums[i]){
+                sum+=nums[i];
             }else{
                 break;
             }
         }
-        int sum1=0;
-        for(int sum:sequential){
-            sum1+=sum;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(sum==nums[i]){
+                sum++;
+            }
         }
-        HashSet<Integer>hs=new HashSet<>(arr);
-        int ans=sum1;
-        while(hs.contains(ans)){
-           ans++;
-        }
-        return ans;
+        return sum;
     }
 }
