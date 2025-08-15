325185bd2acfbfd62664f315e31f8d5e6f7cde28
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-15 14:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (30.26%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -16,15 +16,27 @@
 //     }
 // }
 
-class Solution {
-    public int hammingWeight(int n) {
-        String bin=Integer.toBinaryString(n);
-        int count=0;
-        for(int i=0;i<bin.length();i++){
-            if(bin.charAt(i)=='1'){
-                count++;
+// class Solution {
+//     public int hammingWeight(int n) {
+//         String bin=Integer.toBinaryString(n);
+//         int count=0;
+//         for(int i=0;i<bin.length();i++){
+//             if(bin.charAt(i)=='1'){
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+class Solution{
+    public int hammingWeight(int n){
+        int res=0;
+        for(int i=0;i<32;i++){
+            if(((n>>i)&1)==1){
+                res+=1;
             }
         }
-        return count;
+        return res;
     }
 }
