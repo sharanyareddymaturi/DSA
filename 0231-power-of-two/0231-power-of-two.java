6220dc8a81760f825ca5ccaa9012241e5a7632e8
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-15 10:33:08 +0530

    Time: 1 ms (34.19%), Space: 40.8 MB (66.95%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<0){
+            return false;
+        }
+        for(int i=0;i<31;i++){
+            int ans=(int)Math.pow(2,i);
+            if(n==ans){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
