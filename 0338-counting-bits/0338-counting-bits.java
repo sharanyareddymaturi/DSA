0aa556f3b2493432affdbfcbafcc28124260fa1d
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-08-15 15:37:10 +0530

    Time: 2 ms (93.86%), Space: 46.5 MB (84.12%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,16 +1,26 @@
+// class Solution {
+//     public int[] countBits(int n) {
+//         int arr[]=new int[n+1];
+//         for(int i=0;i<=n;i++){
+//             String bin=Integer.toBinaryString(i);
+//             int count=0;
+            
+//             for(int j=0;j<bin.length();j++){
+//                 if(bin.charAt(j)=='1'){
+//                     count++;
+//                 }
+//             }
+//             arr[i]=count;
+//         }
+//         return arr;
+//     }
+// }
+
 class Solution {
     public int[] countBits(int n) {
         int arr[]=new int[n+1];
         for(int i=0;i<=n;i++){
-            String bin=Integer.toBinaryString(i);
-            int count=0;
-            
-            for(int j=0;j<bin.length();j++){
-                if(bin.charAt(j)=='1'){
-                    count++;
-                }
-            }
-            arr[i]=count;
+            arr[i]=Integer.bitCount(i);
         }
         return arr;
     }
