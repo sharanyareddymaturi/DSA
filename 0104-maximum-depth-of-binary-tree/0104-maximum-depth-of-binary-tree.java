c6904d565d22ec9a9c05ce84da094869b1082872
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-09-20 11:58:31 +0530

    Time: 0 ms (100%), Space: 43 MB (17.27%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -14,29 +14,47 @@
  * }
  */
 
+// class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if(root==null){
+//             return 0;
+//         }
+//         Queue<TreeNode> queue=new LinkedList<>();
+//         queue.add(root);
+//         int depth=0;
+
+//         while(!queue.isEmpty()){
+//             int size=queue.size();
+//             for(int i=0;i<size;i++){
+//                 TreeNode node=queue.poll();//poll==delete
+
+//                 if(node.left==null && node.right==null){
+//                     continue;
+//                 }
+//                 if(node.left!=null){
+//                     queue.add(node.left);
+//                 }
+//                 if(node.right!=null){
+//                     queue.add(node.right);
+//                 }
+//             }
+//             depth++;
+//         }
+//         return depth;
+//     }
+// }
+
 class Solution {
     public int maxDepth(TreeNode root) {
+        return Helper(root,0);
+    }
+    public int Helper(TreeNode root,int curr){
         if(root==null){
-            return 0;
+            return curr;
         }
-        Queue<TreeNode> queue=new LinkedList<>();
-        queue.add(root);
-        int depth=0;
+        int a=Helper(root.left,curr+1);
+        int b=Helper(root.right,curr+1);
 
-        while(!queue.isEmpty()){
-            int size=queue.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=queue.poll();//poll==delete
-
-                if(node.left!=null){
-                    queue.add(node.left);
-                }
-                if(node.right!=null){
-                    queue.add(node.right);
-                }
-            }
-            depth++;
-        }
-        return depth;
+        return Math.max(a,b);
     }
 }
