0866b4577aedb45182c40a561b68ef5706e2d110
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-12-07 22:41:37 +0530

    Time: 3 ms (54.95%), Space: 44.3 MB (56.88%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,23 +1,48 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer>hs1=new HashMap<>();
-        HashMap<Integer,Integer>hs2=new HashMap<>();
-        for(int i=0;i<nums1.length;i++){
-            hs1.put(nums1[i],hs1.getOrDefault(nums1[i],0)+1);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        for(int i=0;i<nums2.length;i++){
-            hs2.put(nums2[i],hs2.getOrDefault(nums2[i],0)+1);
-        }
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int key:hs1.keySet()){
-            if(hs2.containsKey(key)){
-                list.add(key);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                list.add(num);
+                map.remove(num); 
             }
+        }    
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
         }
-        int result[]=new int[list.size()];
-        for(int i=0;i<list.size();i++){
-            result[i]=list.get(i);
-        }
+
         return result;
     }
 }
+
+
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+//         HashMap<Integer,Integer>hs1=new HashMap<>();
+//         HashMap<Integer,Integer>hs2=new HashMap<>();
+//         for(int i=0;i<nums1.length;i++){
+//             hs1.put(nums1[i],hs1.getOrDefault(nums1[i],0)+1);
+//         }
+//         for(int i=0;i<nums2.length;i++){
+//             hs2.put(nums2[i],hs2.getOrDefault(nums2[i],0)+1);
+//         }
+//         ArrayList<Integer> list=new ArrayList<>();
+//         for(int key:hs1.keySet()){
+//             if(hs2.containsKey(key)){
+//                 list.add(key);
+//             }
+//         }
+//         int result[]=new int[list.size()];
+//         for(int i=0;i<list.size();i++){
+//             result[i]=list.get(i);
+//         }
+//         return result;
+//     }
+// }
