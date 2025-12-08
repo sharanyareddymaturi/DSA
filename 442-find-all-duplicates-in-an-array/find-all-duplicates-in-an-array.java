e59d09268721bbe7e156c76265d7f9ee060fa187
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-12-08 23:20:04 +0530

    Time: 5 ms (94.09%) | Memory: 57.6 MB (47.30%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,15 +1,30 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
-        }
         ArrayList<Integer> result=new ArrayList<>();
-        for(int key:hs.keySet()){
-            if(hs.get(key)>1){
-                result.add(key);
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]<0){
+                result.add(index+1);
+            }else{
+                nums[index]=-nums[index];
             }
         }
         return result;
     }
 }
+
+// class Solution {
+//     public List<Integer> findDuplicates(int[] nums) {
+//         HashMap<Integer,Integer> hs=new HashMap<>();
+//         for(int i=0;i<nums.length;i++){
+//             hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
+//         }
+//         ArrayList<Integer> result=new ArrayList<>();
+//         for(int key:hs.keySet()){
+//             if(hs.get(key)>1){
+//                 result.add(key);
+//             }
+//         }
+//         return result;
+//     }
+// }
