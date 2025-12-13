071e26140fd24e44724039078e1e6e20edf5787b
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2025-12-13 11:41:05 +0530

    Time: 12 ms (5.76%) | Memory: 44.3 MB (5.12%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,25 +1,25 @@
-class Solution {
-    public int reverseBits(int n) {
-        String bin="";
-
-        for(int i=0;i<32;i++){
-            bin=bin+(n&1);
-            n>>>=1;
-        }
-        long num=Long.parseLong(bin,2);
-        return (int)num;
-    }
-}
-
 // class Solution {
 //     public int reverseBits(int n) {
-//         String bin = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+//         String bin="";
 
-//         String rev="";
-//         for(int i=bin.length()-1;i>=0;i--){
-//             rev=rev+bin.charAt(i);
+//         for(int i=0;i<32;i++){
+//             bin=bin+(n&1);
+//             n>>>=1;
 //         }
-//         long dec=Long.parseLong(rev,2);
-//         return (int)dec;
+//         long num=Long.parseLong(bin,2);
+//         return (int)num;
 //     }
 // }
+
+class Solution {
+    public int reverseBits(int n) {
+        String bin = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+
+        String rev="";
+        for(int i=bin.length()-1;i>=0;i--){
+            rev=rev+bin.charAt(i);
+        }
+        long dec=Long.parseLong(rev,2);
+        return (int)dec;
+    }
+}
