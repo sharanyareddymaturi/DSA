b49035fa51bf1d79e8f14e0a57bcce4880661d32
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-01-14 17:48:22 +0530

    Time: 0 ms (100%), Space: 61.2 MB (59.98%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void swap(int nums[], int left,int right){
+        while(left<=right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        swap(nums,0,n-1);
+        swap(nums,0,k-1);
+        swap(nums,k,n-1);
+        
+    }
+}
