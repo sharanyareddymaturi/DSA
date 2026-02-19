ed8f16a6cfb0ceb26a3988ed0fdf1912b87728a1
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-02-19 22:08:33 +0530

    Time: 16 ms (5.92%) | Memory: 46.8 MB (34.38%) - LeetSync

--- 2406-decode-the-message/decode-the-message.java
+++ 2406-decode-the-message/decode-the-message.java
@@ -1,6 +1,6 @@
 class Solution {
     public String decodeMessage(String key, String message) {
-        HashMap<Character,Character>hm=new HashMap<>();
+        HashMap<Character,Character> hm=new HashMap<>();
         int temp=97;
         for(int i=0;i<key.length();i++){
             char ch=key.charAt(i);
