d499c04169d1b6e8e4ce306f0143e74594eaeadc
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-02-28 11:36:13 +0530

    Time: 2 ms (3.8%), Space: 45.9 MB (99.02%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -8,31 +8,70 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         ArrayList<Integer> list1=new ArrayList<>();
+//         ArrayList<Integer> list2=new ArrayList<>();
+//         ListNode temp=head;
+//         int index=1;
+//         while(temp!=null){
+//             if(index%2!=0){
+//                 list1.add(temp.val);
+//             }else{
+//                 list2.add(temp.val);
+//             }
+//             temp=temp.next;
+//             index++;
+//         }
+//         ListNode dummy=new ListNode(-1);
+//         ListNode curr=dummy;
+//         for(int i=0;i<list1.size();i++){
+//             curr.next=new ListNode(list1.get(i));
+//             curr=curr.next;
+//         }
+//         for(int i=0;i<list2.size();i++){
+//             curr.next=new ListNode(list2.get(i));
+//             curr=curr.next;
+//         }
+//         return dummy.next;
+//     }
+// }
+
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ArrayList<Integer> list1=new ArrayList<>();
-        ArrayList<Integer> list2=new ArrayList<>();
-        ListNode temp=head;
-        int index=1;
-        while(temp!=null){
-            if(index%2!=0){
-                list1.add(temp.val);
-            }else{
-                list2.add(temp.val);
+        
+        if (head == null)
+            return null;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        ListNode temp = head;
+        int index = 1;
+        while (temp != null) {
+            if (index % 2 != 0) {
+                list.add(temp.val);
             }
-            temp=temp.next;
+            temp = temp.next;
             index++;
         }
-        ListNode dummy=new ListNode(-1);
-        ListNode curr=dummy;
-        for(int i=0;i<list1.size();i++){
-            curr.next=new ListNode(list1.get(i));
-            curr=curr.next;
+        temp = head;
+        index = 1;
+
+        while (temp != null) {
+            if (index % 2 == 0) {
+                list.add(temp.val);
+            }
+            temp = temp.next;
+            index++;
         }
-        for(int i=0;i<list2.size();i++){
-            curr.next=new ListNode(list2.get(i));
-            curr=curr.next;
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        for (int val : list) {
+            curr.next = new ListNode(val);
+            curr = curr.next;
         }
+
         return dummy.next;
     }
 }
