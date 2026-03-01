d944430b9faa06d13e127a4dcec6bca8dbb81b98
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-03-01 22:16:28 +0530

    Time: 3 ms (56.78%), Space: 45.9 MB (87.99%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,21 +1,38 @@
+// class Solution {
+//     public List<String> removeAnagrams(String[] words) {
+//         ArrayList<HashMap<Character,Integer>> list=new ArrayList<>();
+//         for(String w:words){
+//             HashMap<Character,Integer> hm=new HashMap<>();
+//             for(char c:w.toCharArray()){
+//                 hm.put(c,hm.getOrDefault(c,0)+1);
+//             }
+//             list.add(hm);
+//         }
+//         ArrayList<String> res=new ArrayList<>();
+//         res.add(words[0]);
+//         for(int i=1;i<words.length;i++){
+//             if(!list.get(i).equals(list.get(i-1))){
+//                 res.add(words[i]);
+//             }
+//         }
+//         return res;
+
+//     }
+// }
+
 class Solution {
     public List<String> removeAnagrams(String[] words) {
-        ArrayList<HashMap<Character,Integer>> list=new ArrayList<>();
-        for(String w:words){
-            HashMap<Character,Integer> hm=new HashMap<>();
-            for(char c:w.toCharArray()){
-                hm.put(c,hm.getOrDefault(c,0)+1);
-            }
-            list.add(hm);
-        }
         ArrayList<String> res=new ArrayList<>();
         res.add(words[0]);
         for(int i=1;i<words.length;i++){
-            if(!list.get(i).equals(list.get(i-1))){
+            char a[]=words[i].toCharArray();
+            char b[]=res.get(res.size()-1).toCharArray();
+            Arrays.sort(a);
+            Arrays.sort(b);
+            if(!Arrays.equals(a,b)){
                 res.add(words[i]);
             }
         }
         return res;
-
     }
 }
