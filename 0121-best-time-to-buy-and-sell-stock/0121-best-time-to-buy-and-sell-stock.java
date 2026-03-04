dfd995c55900071be046ef6d80ab9e5af300f282
Maturi sharanya <165792309+sharanyareddymaturi@users.noreply.github.com>
2026-03-04 13:56:39 +0530

    Time: 2 ms (41.19%), Space: 94.5 MB (50.35%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,33 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int max=0;
+        int ans=0;
         int low=prices[0];
         for(int i=1;i<n;i++){
             int temp=prices[i]-low;
-            max=Math.max(max,temp);
+            ans=Math.max(temp,ans);
             if(prices[i]<low){
                 low=prices[i];
             }
         }
-        return max;
+        return ans;
     }
 }
+
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//        int max=0;
+//        int n=prices.length;
+//        for(int i=0;i<n;i++){
+//         int temp=0;
+//         for(int j=i;j<n;j++){
+//             if(prices[j]>prices[i]){
+//                 temp=prices[i]-prices[j];
+//             }
+//             max=Math.max(max,Math.abs(temp));
+//         }
+//        } 
+//        return max;
+//     }
+// }
